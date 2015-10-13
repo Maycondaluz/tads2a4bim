2496fb4de76cf2a7d3dd4bdc82e4af6be9e57fea
Maycon <mayconvluz@gmail.com>
2015-10-13 20:03:51 -0300

    Alteraçao dependencia h2 xml e criaçao da classe ClienteH2Impl

---
+++ src/main/java/br/univel/cadastro/model/ClienteDaoH2Impl.java
@@ -0,0 +1,45 @@
+package br.univel.cadastro.model;
+
+import java.util.List;
+
+public class ClienteDaoH2Impl implements ClienteDao{
+
+	@Override
+	public void inserir(Cliente c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Cliente c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Cliente c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cliente buscar(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente buscarPorExemplo(Cliente c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	 
+
+}
